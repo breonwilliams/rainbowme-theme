6781314f7f20dcca3ffa64cb2f7a90be5684d4e2
breonw <breon_williams@unc.edu>
2016-04-15 14:00:25 -0400

    modify gravatar

--- functions.php
+++ functions.php
@@ -60,12 +60,50 @@ if (!function_exists('bootstrapBasicSetup')) {
 }
 add_action('after_setup_theme', 'bootstrapBasicSetup');
 
+// avatar
 add_filter( 'avatar_defaults', 'newgravatar' );
 function newgravatar ($avatar_defaults) {
 	$myavatar = get_bloginfo('template_directory') . '/img/avatar.png';
 	$avatar_defaults[$myavatar] = "RainbowMe Avatar";
 	return $avatar_defaults;
 }
+
+function bp_remove_gravatar ($image, $params, $item_id, $avatar_dir, $css_id, $html_width, $html_height, $avatar_folder_url, $avatar_folder_dir) {
+
+	$default = get_stylesheet_directory_uri() .'/img/avatar.png';
+
+	if( $image && strpos( $image, "gravatar.com" ) ){
+
+		return '<img src="' . $default . '" alt="avatar" class="avatar" ' . $html_width . $html_height . ' />';
+	} else {
+		return $image;
+
+	}
+
+}
+add_filter('bp_core_fetch_avatar', 'bp_remove_gravatar', 1, 9 );
+
+function remove_gravatar ($avatar, $id_or_email, $size, $default, $alt) {
+
+	$default = get_stylesheet_directory_uri() .'/img/avatar.png';
+	return "<img alt='{$alt}' src='{$default}' class='avatar avatar-{$size} photo avatar-default' height='{$size}' width='{$size}' />";
+}
+
+add_filter('get_avatar', 'remove_gravatar', 1, 5);
+
+function bp_remove_signup_gravatar ($image) {
+
+	$default = get_stylesheet_directory_uri() .'/img/avatar.png';
+
+	if( $image && strpos( $image, "gravatar.com" ) ){
+
+		return '<img src="' . $default . '" alt="avatar" class="avatar" width="150" height="150" />';
+	} else {
+		return $image;
+	}
+
+}
+add_filter('bp_get_signup_avatar', 'bp_remove_signup_gravatar', 1, 1 );
 
 if (!function_exists('bootstrapBasicWidgetsInit')) {
 	/**
