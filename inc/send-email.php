056f5ac0e334d4246bd8cf89aff9baf26f56e1f8
breonw <breon_williams@unc.edu>
2016-04-16 15:17:35 -0400

    modify reply email

--- inc/send-email.php
+++ inc/send-email.php
@@ -1,8 +1,16 @@
 <?php
 
-/* enter the full email address you want displayed */
-/* from https://rainbomekids.com/ */
+/* auto-detect the server so you only have to enter the front/from half of the email address, including the @ sign */
 function xyz_filter_wp_mail_from($email){
-    return "hello@rainbowmekids.com";
+    /* start of code lifted from wordpress core, at http://svn.automattic.com/wordpress/tags/3.4/wp-includes/pluggable.php */
+    $sitename = strtolower( $_SERVER['SERVER_NAME'] );
+    if ( substr( $sitename, 0, 4 ) == 'www.' ) {
+        $sitename = substr( $sitename, 4 );
+    }
+    /* end of code lifted from wordpress core */
+    $myfront = "hello@";
+    $myback = $sitename;
+    $myfrom = $myfront . $myback;
+    return $myfrom;
 }
 add_filter("wp_mail_from", "xyz_filter_wp_mail_from");
