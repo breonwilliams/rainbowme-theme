8cd95412e81bf299cb4b12f81830c2e1cd9127a6
breonw <breon_williams@unc.edu>
2016-04-16 15:32:35 -0400

    modify reply email

--- inc/send-email.php
+++ inc/send-email.php
@@ -1,16 +1,12 @@
 <?php
 
-/* auto-detect the server so you only have to enter the front/from half of the email address, including the @ sign */
-function xyz_filter_wp_mail_from($email){
-    /* start of code lifted from wordpress core, at http://svn.automattic.com/wordpress/tags/3.4/wp-includes/pluggable.php */
-    $sitename = strtolower( $_SERVER['SERVER_NAME'] );
-    if ( substr( $sitename, 0, 4 ) == 'www.' ) {
-        $sitename = substr( $sitename, 4 );
-    }
-    /* end of code lifted from wordpress core */
-    $myfront = "hello@";
-    $myback = $sitename;
-    $myfrom = $myfront . $myback;
-    return $myfrom;
+// Change default WordPress email address
+add_filter('wp_mail_from', 'new_mail_from');
+add_filter('wp_mail_from_name', 'new_mail_from_name');
+
+function new_mail_from($old) {
+    return 'hello@rainbowmekids.com';
 }
-add_filter("wp_mail_from", "xyz_filter_wp_mail_from");
+function new_mail_from_name($old) {
+    return 'RainbowMe';
+}
